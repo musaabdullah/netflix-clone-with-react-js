b121b3b1b9ef928f1bf227d2bfdc84d04224fb4e
musaabdullah <musaabdalwahed@gmail.com>
2021-02-27 16:51:47 +0200

    request from netflix nav black

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './nav.css';
+
 function Nav() {
+    
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if(window.scrollY > 100) {
+                 handleShow(true);
+                }
+                else {
+                    handleShow(false)
+                }  
+        });
+        return () => {
+            window.removeEventListener("scroll");
+        };
+    }, [])
+
     return (
-        <div className="nav">
+        <div className={`nav ${show && "nav__black"}`}>
             <img 
             className="nav__logo"
             src="https://pngimg.com/uploads/netflix/netflix_PNG11.png" 
